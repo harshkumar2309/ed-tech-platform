0972c746816e1752efda2707c5ed296f39762cfa
Harsh Kumar <yadavharsh1201@gmail.com>
2026-05-26 21:39:30 +0530

    feat: add middleware for Admin role.

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,61 +1,62 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-dotenv.config();  
-import { User } from "../models/User"  
+dotenv.config();
+import { User } from "../models/User";
 
 // auth middleware
 export const auth = async (req, res, next) => {
-    try{
-        // extract token
-        const token = req.cookies.token
-                        || req.body.token 
-                        || req.header("Authorisation").replace("Bearer", "");
+  try {
+    // extract token
+    const token =
+      req.cookies.token ||
+      req.body.token ||
+      req.header("Authorisation").replace("Bearer", "");
 
-        // if token is missing
-        if(!token){
-            return res.status(401).json({
-                success: false,
-                message: "Token is missing",
-            });
-        }
-
-        // verify token
-        try{
-            const decode = await jwt.verify(token, process.env.JWT_SECRET);
-            console.log(decode);
-            req.user = decode; 
-        } catch(error){
-            return res.status(401).json({
-              success: false,
-              message: "Token is invalid",
-            });
-        }
-        next();
-    } catch(error){
-        return res.status(401).json({
-          success: false,
-          message: "Something went wrong, while verifying the token",
-        });
+    // if token is missing
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Token is missing",
+      });
     }
-}
+
+    // verify token
+    try {
+      const decode = await jwt.verify(token, process.env.JWT_SECRET);
+      console.log(decode);
+      req.user = decode;
+    } catch (error) {
+      return res.status(401).json({
+        success: false,
+        message: "Token is invalid",
+      });
+    }
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Something went wrong, while verifying the token",
+    });
+  }
+};
 
 // isStudent
 export const isStudent = async (req, res, next) => {
-    try{
-        if(req.user.accountType !== "Student"){
-            return res.status(401).json({
-                success: false,
-                message: 'This is a protected route for Students only',
-            });
-        }
-        next();
-    } catch(error){
-        return res.status(500).json({
-            success: false,
-            message: 'User role cannot be verified, please try again',
-        })
+  try {
+    if (req.user.accountType !== "Student") {
+      return res.status(401).json({
+        success: false,
+        message: "This is a protected route for Students only",
+      });
     }
-} 
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "User role cannot be verified, please try again",
+    });
+  }
+};
 
 // isInstructor
 export const isInstructor = async (req, res, next) => {
@@ -76,3 +77,19 @@ export const isInstructor = async (req, res, next) => {
 };
 
 // isAdmin
+export const isAdmin = async (req, res, next) => {
+  try {
+    if (req.user.accountType !== "isAdmin") {
+      return res.status(401).json({
+        success: false,
+        message: "This is a protected route for Admin only",
+      });
+    }
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "User role cannot be verified, please try again",
+    });
+  }
+}
